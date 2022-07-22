24b1382fe4a193d5933ef0e2401de670deb62983
sebmatecho <slozanof@unal.edu.co>
2022-07-22 18:55:05 -0500

    app test

--- Dashboard.py
+++ Dashboard.py
@@ -112,16 +112,16 @@ def mapa1(data,width=1100, height=750):
      data_aux = data[['id','zipcode']].groupby('zipcode').count().reset_index()
      custom_scale = data_aux['id'].quantile([0,0.2,0.4,0.6,0.8,1]).tolist()
      
-     mapa = folium.Map(location=[data['lat'].mean(), data['long'].mean()], zoom_start=8)
-     folium.Choropleth(
-                         geo_data=geofile, 
-                         data=data_aux,
-                         key_on='feature.properties.ZIPCODE',
-                         columns=['zipcode', 'id'],
-                         threshold_scale=custom_scale,
-                         fill_color='YlOrRd',
-                         highlight=True).add_to(mapa)
-     folium_static(mapa, width=0.45*width, height=0.45*width)
+     # mapa = folium.Map(location=[data['lat'].mean(), data['long'].mean()], zoom_start=8)
+     # folium.Choropleth(
+     #                     geo_data=geofile, 
+     #                     data=data_aux,
+     #                     key_on='feature.properties.ZIPCODE',
+     #                     columns=['zipcode', 'id'],
+     #                     threshold_scale=custom_scale,
+     #                     fill_color='YlOrRd',
+     #                     highlight=True).add_to(mapa)
+     # folium_static(mapa, width=0.45*width, height=0.45*width)
      return None
 
 def mapa2(data,geo_info,width=1100, height=750):
