f123ec29e99d9e0e58dc195bb7df0ee8e0f19a29
sebmatecho <slozanof@unal.edu.co>
2022-07-23 11:58:33 -0500

    test app

--- Dashboard.py
+++ Dashboard.py
@@ -110,7 +110,7 @@ def mapa1(data,width=1100, height=750):
      data_aux = data[['id','zipcode']].groupby('zipcode').count().reset_index()
      custom_scale = data_aux['id'].quantile([0,0.2,0.4,0.6,0.8,1]).tolist()
      geodata = get_geofile( )
-     test_list = data_aux['zipcode'].to_list()
+     test_list = list(set(data_aux['zipcode']))
      geodata = geodata[geodata['ZIP'].isin(test_list)]
      mapa = folium.Map(location=[data['lat'].mean(), data['long'].mean()], zoom_start=8)
      folium.Choropleth(geo_data= geodata, 
